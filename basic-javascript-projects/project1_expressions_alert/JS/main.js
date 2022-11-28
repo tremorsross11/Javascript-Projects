307fecf08fbf2a4f11eaebca421b27d3cbcc5794
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-11-28 12:33:28 -0800

    Basic Javascript Testing

---
+++ basic-javascript-projects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+window.alert("Hello, World!"); //Making a button
+document.write("testing");  //Displaying string on a document
+var test = "Testing Testing"; //Assigning a variable to a string
+document.write(test);
+var test1 = "I hope this works";
+window.alert(test1);
+document.write("The old man said \"Watch out for the white whale\""); //adding qoutes
+document.write("\"No Windows I dont want to turn on sticky key\'s"
+ + " and stop asking me all the time\"" );
+ var constring = ("\"I am assigning a concentated string value"
+ + " to a variable\""); //Displaying a concentated string on a document
+ document.write(constring);
+ var element1 = "Fire", element2 = "Water", element3 = "Earth"; //Assigning multiple variables
+ document.write(element2);
+ document.write(72-43); //Displaying the end result of a math function
+ var Sent1 = "This is the beginning of my string", Sent2 = " and this is the end";
+ function add_string() { //Made a function that adds two string variables together
+    return(Sent1+Sent2);
+ }
+document.write(add_string());
